5191c1384842e9ef0dff2f5a205f3b9cf3bfd2ea
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-18 10:20:16 +0300

    add graphql schema

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const app = require('express')();
 const cors = require('cors');
 const helmet = require('helmet');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./Schema/schema');
 
 const PORT = process.env.PORT || 3001;
 
 // Middleware
 
 app.use(cors());
-app.use(helmet());
-app.use('/graphql', graphqlHTTP());
+app.use(
+  helmet({
+    // Todo remove unsafe options
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self' 'unsafe-inline' 'unsafe-eval'"],
+        styleSrc: ["'self' 'unsafe-inline'"],
+      },
+    },
+  })
+);
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 // Test
 
