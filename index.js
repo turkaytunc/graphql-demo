8017b3063b877d247ac84ee89396994a2d5b1a50
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-18 08:33:31 +0300

    add cors helmet middleware

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
 const app = require('express')();
 const cors = require('cors');
+const helmet = require('helmet');
 
 const PORT = process.env.PORT || 3001;
+
+// Middleware
+
+app.use(cors());
+app.use(helmet());
+
+// Test
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello From Express');
+});
 
 app.listen(PORT, () => {
   console.log(`Server listen on port ${PORT}.`);
