67f3d8992f53ffb58c08a608b1f602d3f9b1cf32
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-19 18:16:13 +0300

    add mongodb uri and mongoose

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./Schema/schema');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
+const DBUSERNAME = process.env.DBUSERNAME;
+const DBPASSWORD = process.env.DBPASSWORD;
+const DBNAME = 'gQL';
+const DBURI = `mongodb+srv://${DBUSERNAME}:${DBPASSWORD}@codecamp-graphql.ihj2d.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
 
 // Middleware
 
